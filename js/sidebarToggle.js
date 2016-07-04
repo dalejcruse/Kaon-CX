e13f9eab97c0041c6094577e9f0d763b811705da
Dale Cruse <dale.cruse@gmail.com>
2016-07-04 09:46:20 -0700

    Moved file, updated path

--- js/sidebarToggle.js
+++ js/sidebarToggle.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	$(".toggle").toggle(
 	      function(){
 	      	$(".main p").css({"display":"block"});
-	      	$(".toggle").css({"background":"url(_img/buttonRedPlusMinus.png) no-repeat left bottom"});
+	      	$(".toggle").css({"background":"url(buttonRedPlusMinus.png) no-repeat left bottom"});
 	      },
 	      function () {
 	      	$(".main p").css({"display":"none"});
-	      	$(".toggle").css({"background":"url(_img/buttonRedPlusMinus.png) no-repeat left top"});
+	      	$(".toggle").css({"background":"url(buttonRedPlusMinus.png) no-repeat left top"});
 	      }
 	    );
 	   }); 
